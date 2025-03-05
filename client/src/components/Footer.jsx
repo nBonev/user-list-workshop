8f05cf3011be116d8fa3295d59dbf765be80ae27
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-03-05 15:58:49 +0200

    Extract more components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer class="footer">
+        <footer className="footer">
             <p>This site is designed to be used for training purposes at SoftUni.</p>
         </footer>
     );
